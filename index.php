a9ef8863e2b4934d9a9da4b25882fa907eb6379b
Matheus Siqueira <siqueira4503@gmail.com>
2021-05-17 01:50:00 -0300

    Continuação módulo variáveis

--- index.php
+++ index.php
@@ -105,6 +105,26 @@
                                 Desafio Equação
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=atribuicoes">
+                                Atribuições
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=interpolacao">
+                                Interpolação
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">
+                                Variáveis Variáveis
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=desafio_variaveis">
+                                Desafio Variáveis
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
